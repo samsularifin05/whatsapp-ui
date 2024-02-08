48db9d211fb810465968737aaa237d42efb4b1ce
samsularifin05 <samsul026@gmail.com>
2024-02-08 17:11:11 +0700

    update README.md

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,25 @@ import {
 } from "./components";
 
 const App = () => {
-  const [progress, setProgress] = useState(100);
+  const [progress, setProgress] = useState(0);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Increment progress by 1 every 100 milliseconds until it reaches 100
       if (progress < 100) {
         setProgress(progress + 1);
       } else {
         clearInterval(interval);
-        setLoading(false); // Hide progress bar and show panel
+        setLoading(false);
       }
     }, 100);
 
-    // Cleanup function to clear interval on unmount or progress reaches 100
     return () => clearInterval(interval);
   }, [progress]);
   return loading ? (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-color2">
       <img src={Logo} className="h-72 w-72" />
-      <div className="-mt-20 w-[25rem] rounded-lg bg-gray-200">
+      <div className="-mt-20 w-[18rem] rounded-lg bg-gray-200 xs:w-[10rem] md:w-[25rem] lg:w-[25rem]">
         <div
           className="h-1 rounded-lg bg-color6"
           style={{ width: `${progress}%` }}
