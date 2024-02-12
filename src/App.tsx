7f71ce88fb96b3922ffeb4fe36e65e4b8a238238
samsularifin05 <samsul026@gmail.com>
2024-02-12 17:58:44 +0700

    perbaikan loading

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,24 @@ import {
 const App = () => {
   const [progress, setProgress] = useState(0);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (progress < 100) {
-        setProgress(progress + 1);
-      } else {
-        clearInterval(interval);
-        setLoading(false);
-      }
-    }, 100);
-    return () => clearInterval(interval);
+    const storedIsLoading = localStorage.getItem("IsLoading");
+    if (storedIsLoading === "false") {
+      setLoading(false);
+    } else {
+      const interval = setInterval(() => {
+        if (progress < 100) {
+          setProgress(progress + 1);
+          localStorage.setItem("IsLoading", "true");
+        } else {
+          clearInterval(interval);
+          setLoading(false);
+          localStorage.setItem("IsLoading", "false");
+        }
+      }, 100);
+      return () => clearInterval(interval);
+    }
   }, [progress]);
   return loading ? (
     <LoadingScreen progress={progress} />
