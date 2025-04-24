d206af8367cdf8aa676d63a485d5ea88010ecd2e
samsularifin05 <samsul026@gmail.com>
2025-04-24 10:33:51 +0700

    add pwa

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -2,5 +2,7 @@ import BgIntro from "./intro.png";
 import ExampleProfile from "./profile.jpg";
 import BgChat from "./bg-chat.png";
 import BarcodeImg from "./barcode.png";
+import icon192 from "./icon-192.png";
+import icon512 from "./icon-512.png";
 
-export { BgIntro, ExampleProfile, BgChat, BarcodeImg };
+export { BgIntro, icon512, icon192, ExampleProfile, BgChat, BarcodeImg };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,82 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: "production",
+  base: "/",
+  includeAssets: ["favicon.svg"],
+  registerType: "autoUpdate",
+  workbox: {
+    clientsClaim: false,
+    skipWaiting: true,
+    disableDevLogs: true,
+    maximumFileSizeToCacheInBytes: 6000000,
+  },
+  manifest: {
+    name: "WhatsApp",
+    short_name: "WhatsApp",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "assets/images/icon-192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "assets/images/icon-512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+
+  devOptions: {
+    enabled: false,
+    type: "module",
+    navigateFallback: "index.html",
+  },
+};
+const timestamp = new Date().getTime();
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(pwaOptions)],
+  build: {
+    minify: "esbuild",
+    emptyOutDir: true,
+    outDir: "dist",
+    sourcemap: false,
+    cssCodeSplit: true,
+    modulePreload: true,
+    chunkSizeWarningLimit: 1000000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        chunkFileNames: `assets/js/[hash]-${timestamp}.js`,
+        entryFileNames: `assets/js/[hash]-${timestamp}.js`,
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg|webp|avif)$/.test(name ?? "")) {
+            // return `assets/images/[hash]-${timestamp}[extname]`;
+            return `assets/images/${name}`;
+          }
+          if (/\.(mp3)$/.test(name ?? "")) {
+            return `assets/mp3/${name}`;
+          }
+          if (/\.(ttf|woff2|svg)$/.test(name ?? "")) {
+            // return `assets/font/[hash]-${timestamp}[extname]`;
+            return `assets/font/${name}`;
+          }
+          if (/\.css$/.test(name ?? "")) {
+            return `assets/css/[hash]-${timestamp}[extname]`;
+          }
+          return `assets/${name}`;
+        },
+      },
+    },
+  },
 });
